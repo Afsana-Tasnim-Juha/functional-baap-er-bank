3447bb0a02793a3fbd83e921855d2bbcd2560094
Afsana-Tasnim-Juha <111423065+Afsana-Tasnim-Juha@users.noreply.github.com>
2023-08-23 11:28:59 +0200

    function added

--- js/deposit.js
+++ js/deposit.js
@@ -1,3 +1,12 @@
-document.getAnimations('btn-deposit').addEventListener('click', function () {
+function getInputFieldValueById(inputFieldId) {
+    const inputField = document.getElementById(inputFieldId);
+    const inputFieldValueString = inputField.value;
+    const inputFieldValue = parseFloat(inputFieldValueString);
+    inputField.value = '';
+    return inputFieldValue;
+}
 
+
+document.getAnimations('btn-deposit').addEventListener('click', function () {
+    const newDepositAmount = getInputFieldValueById('deposit-field');
 })
