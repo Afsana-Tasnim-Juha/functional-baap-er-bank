0754128b5f3353f6d509d62c97908adb07d2c634
Afsana-Tasnim-Juha <111423065+Afsana-Tasnim-Juha@users.noreply.github.com>
2023-08-23 12:17:53 +0200

    balance added

--- js/deposit.js
+++ js/deposit.js
@@ -7,13 +7,25 @@ function getInputFieldValueById(inputFieldId) {
 }
 function getElementValueById(elementId) {
     const element = document.getElementById(elementId);
-    const elementValueString = element.value;
+    const elementValueString = element.innerText;
     const value = parseFloat(elementValueString);
-    element.value = '';
+
     return value;
 }
 
+function setElementById(elementId, newValue) {
+    const element = document.getElementById(elementId);
+    element.innerText = newValue;
 
-document.getAnimations('btn-deposit').addEventListener('click', function () {
+}
+
+
+document.getElementById('btn-deposit').addEventListener('click', function () {
     const newDepositAmount = getInputFieldValueById('deposit-field');
+    const previousDepositTotal = getElementValueById('deposit-total');
+    const newDepositTotal = previousDepositTotal + newDepositAmount;
+    setElementById('deposit-total', newDepositTotal);
+    const previousBalanceTotal = getElementValueById('balance-total');
+    const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+    setElementById('balance-total', newBalanceTotal);
 })
